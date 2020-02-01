f17565b586e27a28a4f0fa79b506e8d9b538076a
Angelo Ibarrola <AngeloFolder@angelos-macbook-pro.local>
2020-01-31 18:09:52 -0800

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Angelo Ibarrola on 1/31/20.
+//  Copyright © 2020 Angelo Ibarrola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
